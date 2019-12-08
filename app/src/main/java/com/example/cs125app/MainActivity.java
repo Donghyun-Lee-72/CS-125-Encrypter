8a6d625decf53bc8363ca5b774860b434d249107
Donghyun Lee <dhlee991124@gmail.com>
2019-12-07 18:09:27 -0600

    remove activity_main.xml

--- app/src/main/java/com/example/cs125app/MainActivity.java
+++ app/src/main/java/com/example/cs125app/MainActivity.java
@@ -11,7 +11,6 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     protected void onCreate(final Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.home);

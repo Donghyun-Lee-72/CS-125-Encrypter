86ccb87353ca9b2afc8164f319f79bb80553fe76
Donghyun Lee <dhlee991124@gmail.com>
2019-12-09 13:19:36 -0600

    fixing

--- app/src/main/java/com/example/cs125app/MainActivity.java
+++ app/src/main/java/com/example/cs125app/MainActivity.java
@@ -15,24 +15,15 @@ public class MainActivity extends AppCompatActivity {
         //initial GUI interface is the home
         setContentView(R.layout.home);
 
-//        Text input to encrypt or Encrypted input to decrypt
-//        TextView inputBox = findViewById(R.id.text);
-//        final String textInput = inputBox.getText().toString();
-//
-//        //key used for decryption/encryption
-//        TextView key = findViewById(R.id.number);
-//        String keyAsString = key.getText().toString();
-//        final int keyAsInt = Integer.parseInt(keyAsString);
-//
         Button encryptionMode = findViewById(R.id.encryptionMode);
+        Button decryptionMode = findViewById(R.id.decryptionMode);
+
         encryptionMode.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, Encrypt.class));
             }
         });
 
-
-        Button decryptionMode = findViewById(R.id.decryptionMode);
         decryptionMode.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, Decrypt.class));
@@ -40,27 +31,27 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void startEncrypt() {
-        //turn to the results page and show the result
-        Intent intent = new Intent(this, Encrypt.class);
-        //make an intent of the other resultActivity
-
-    }
-
-    private String encryptText(String input, int key) {
-        //encryption algorithm
-        return "Hello, World!";
-    }
-    private void startDecrypt() {
-        //turn to the results page and show the result
-        Intent intent = new Intent(this, Encrypt.class);
-        //make an intent of the other resultActivity
-
-    }
-
-    private String decryptText(String input, int key) {
-        //decryption algorithm
-        return "Hello, World!";
-    }
+//    private void startEncrypt() {
+//        //turn to the results page and show the result
+//        Intent intent = new Intent(this, Encrypt.class);
+//        //make an intent of the other resultActivity
+//
+//    }
+//
+//    private String encryptText(String input, int key) {
+//        //encryption algorithm
+//        return "Hello, World!";
+//    }
+//    private void startDecrypt() {
+//        //turn to the results page and show the result
+//        Intent intent = new Intent(this, Encrypt.class);
+//        //make an intent of the other resultActivity
+//
+//    }
+//
+//    private String decryptText(String input, int key) {
+//        //decryption algorithm
+//        return "Hello, World!";
+//    }
 }
 
